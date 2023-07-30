79f34a18ba6a3f423423c513a3a70c243790d90d
Javier López Rodríguez <javierlopezbq@gmail.com>
2023-07-30 19:47:18 +0200

    removed logging from main

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as flower_router
-import logging
 
 config = dotenv_values(".env")
 # converts the .env file contents into a dictionary
 
 app = FastAPI()
-# Set the logging level to DEBUG for the FastAPI application
-# Set up the logging configuration
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.DEBUG)
 
 # Event handler to connect to the Atlas MongoDB cluster on startup
 @app.on_event("startup")
